666eb55dc46eb493d4d6c9c1f159cd781c29f0bc
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-07-22 10:11:11 +0530

    uploading functionality

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,94 @@
+
+import { useDropzone } from 'react-dropzone';
+import React, { useCallback, useState } from 'react'
+
+
+interface FileUploaderProps {
+    onFileSelect?: (file: File|null) =>void;
+}
+
+const FileUploader = ({onFileSelect}: FileUploaderProps) => {
+
+
+
+    const onDrop = useCallback((acceptedFiles: File[])=>{
+        const file = acceptedFiles[0] || null;
+        onFileSelect?.(file);
+
+
+
+    }, [onFileSelect]);
+
+    const maxFileSize = 20*1024*1024
+
+    const {getRootProps, getInputProps, isDragActive, acceptedFiles}  = useDropzone({onDrop, 
+      multiple:false,
+      accept:{'application/pdf': ['.pdf'] },
+      maxSize: maxFileSize,
+    });
+
+    
+
+
+    const file = acceptedFiles[0] || null;
+
+        // Converts bytes to a human-readable string (KB, MB, GB)
+    function formatSize(bytes: number): string {
+      if (bytes < 1024) return `${bytes} B`;
+      const kb = bytes / 1024;
+      if (kb < 1024) return `${kb.toFixed(2)} KB`;
+      const mb = kb / 1024;
+      if (mb < 1024) return `${mb.toFixed(2)} MB`;
+      const gb = mb / 1024;
+      return `${gb.toFixed(2)} GB`;
+    }
+
+
+
+  return (
+    <div className=' w-full gradient-border'>
+        <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div className=' space-y-4 cursor-pointer'>
+        
+
+        {file?(
+
+          <div className=' uploader-selected-file' onClick={(e) =>e.stopPropagation()}>
+              <img src="/images/pdf.png" alt="pdf" className='size-10' />
+            <div className=' flex items-center space-x-3'>
+              <div>
+                <p className=' text-sm font-medium text-gray-700 truncate max-w-xs'>{file.name}</p>
+                <p className=' text-sm text-gray-500'>{formatSize(file.size)}</p>
+
+              </div>
+            </div>
+            <button className=' p-2 cursor-pointer' onClick={(e)=>{
+              onFileSelect?.(null);
+            }}>
+              <img src="/icons/cross.svg" alt="remove" className=' w-4 h-4'/>
+            </button>
+
+          </div>
+            
+        ):(
+            <div>
+              <div className=' mx-auto w-16 h-16 flex items-center justify-center mb-2'>
+            <img src="/icons/info.svg" alt="upload" className=' size-20'/>
+        </div>
+            <p className=' text-lg text-gray-500'>
+                <span className=' font-semibold'>
+                    Click to upload
+                </span> or Drag and Drop
+            </p>
+            <p className=' text-lg text-gray-500'>PDF (max {formatSize(maxFileSize)})</p>
+            </div>
+        )}
+
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default FileUploader
---
+++ app/routes/upload.tsx
@@ -0,0 +1,98 @@
+import React, { useState, type FormEvent } from 'react'
+import FileUploader from '~/components/FileUploader';
+import Navbar from '~/components/Navbar'
+
+const upload = () => {
+
+    const [isPorcessing, setIsProcessing] = useState(false);
+
+    const [statustext, setstatustext] = useState('')
+
+    const [file, setfile] = useState<File | null>(null);
+
+    
+
+    const handleFileSelect = (file: File | null) =>{
+        setfile(file);
+    }
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        const form = e.currentTarget.closest('form'); //to get from data without reload
+        if(!form) return;
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name')
+        const jobTitle = formData.get('job-title')
+        const jobDescription = formData.get('job-description')
+
+        console.log({
+            companyName, jobTitle, jobDescription, file
+        });
+        
+    }
+    
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">  
+        <div className=' page-heading py-16'>
+            <h1>
+                Smart feedback for your dream job
+            </h1>
+
+            {isPorcessing ? (
+                <>
+                    <h2>{statustext}</h2>
+                    <img src="/images/resume-scan.gif" alt="" className='w-full' />
+                </>
+            ):(
+                <h2>
+                    Drop your resume for your ATS score and imporvement tips
+                </h2>
+            )}
+
+            {
+                !isPorcessing && (
+                    <form id='upload-form' onSubmit={handleSubmit} className='flex flex-col gap-4 mt-8'>
+
+                        <div className=' form-div'>
+                            <label htmlFor="company-name">Company Name</label>
+                            <input type="text" name='company-name' placeholder='Company Name' id='compnay-name' />
+
+                        </div>
+                        <div className=' form-div'>
+                            <label htmlFor="job-title">Job Title</label>
+                            <input type="text" name='job-title' placeholder='Job Title' id='job-title' />
+
+                        </div>
+                        <div className=' form-div'>
+                            <label htmlFor="job-description">Job Description</label>
+                            <textarea rows={5} name='job-description' placeholder='Job Description' id='job-description' />
+
+                        </div>
+                        <div className=' form-div'>
+                            <label htmlFor="uploader">Upload Resume</label>
+                                <FileUploader onFileSelect={handleFileSelect}/>
+
+                        </div>
+
+                        <button className=' primary-button' type='submit'>
+
+                            Analyze Resume
+
+                        </button>
+
+                    </form>
+                )
+            }
+
+        </div>
+    </section>
+    
+    </main>
+  )
+}
+
+export default upload
